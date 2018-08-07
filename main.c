7bcfd4f2ec73fb47537640369e01376832646998
ScheduleManagement <42159400+ScheduleManagement@users.noreply.github.com>
2018-08-07 10:00:54 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,11 @@
-#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
 
-/* run this progr대머리빡빡이!am using the console pauser or add your own getch, system("pause") or input loop */
-
-int chec(int a){
-	return 0;
-}
-
-int chec(int a,int b){
-	return a;
-}
-int main() {
-	return 0;
-}
+struct ITEM {
+	
+	int budget[10];
+	
+	int item[
+	int price[5];
+};
