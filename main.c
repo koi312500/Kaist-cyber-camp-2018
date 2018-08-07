14e2b7a35a52c784c2f29a3856e44ab153540028
gunwoo7 <b9635005@naver.com>
2018-08-07 09:53:23 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int chec(int a){
+	return 0;
+}
+
+int chec(int a,int b){
+	return a;
+}
+int main() {
+	return 0;
+}
